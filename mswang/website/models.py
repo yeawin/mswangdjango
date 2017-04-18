760555c200cf87b3e6b2e0a3b6d55c8d342e3fe2
yeawin <ywliu@xmu.edu.cn>
2017-04-18 09:08:22 +0800

    目录模型

--- mswang/website/models.py
+++ mswang/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+	title = models.CharField(max_length=20, null=False, help_text='板块名')
+	parent = models.IntegerField(null=False, help_text='上级目录')
+	grade = models.IntegerField(null=False, help_text='级别')
+
+
